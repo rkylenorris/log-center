d17dad33ec602d2e91a7231e9b28b51926f4cba3
Kyle Norris <r.kyle.norris@gmail.com>
2025-03-23 17:30:33 -0400

    updated _init__ file to load environemnt varaibles for module to use

--- log_center/__init__.py
+++ log_center/__init__.py
@@ -1,3 +1,7 @@
+from dotenv import load_dotenv
+#load environemnt variables from .env file
+load_dotenv()
+
 from .api import router
 from .models import LogEntry, APIKey, LogLevel, get_db
 from .log_client import LogWriter
